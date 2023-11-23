22b30e7d9f3e9690c4975e1271aa3be537544981
Arman Ommid <armanommid@gmail.com>
2023-11-23 02:11:58 -0800

    push1

---
+++ lstm.py
@@ -0,0 +1,82 @@
+from abc import ABC
+
+import os
+import torch
+import torch.nn as nn
+
+WEIGHTS_EXTENSION = ".pth"
+
+class _Network(nn.Module, ABC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def load(self, weights_path: str=None, map_location: str="cpu"):
+        """
+        Load weights if weights were specified
+        """
+        if not weights_path: return
+        self.load_state_dict(torch.load(weights_path, map_location=torch.device(map_location)))
+
+    def save(self, path: str, tag: str, filename: str="weights.pth"):
+        """
+        All saves should be under the same path folder, under different tag folders, with the same filename
+        """
+        filename = filename.split(".")[0] + WEIGHTS_EXTENSION
+        save_path = os.path.join(path, tag, filename)
+        torch.save(self.state_dict(), save_path)
+
+class _LSTM(_Network):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def init_hidden(self, batch_size):
+        device = next(self.parameters()).device
+        h0 = torch.zeros(self.num_layers, batch_size, self.h_dim, device=device)
+        c0 = torch.zeros(self.num_layers, batch_size, self.h_dim, device=device)
+        return (h0, c0)
+
+class LSTMGenerator(_LSTM):
+    def __init__(self, h_dim=64, dim=4, num_layers=4, **kwargs):
+        super().__init__(**kwargs)
+
+        self.dim = dim
+        self.h_dim = h_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(h_dim, h_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(h_dim, dim)
+
+    def forward(self, z, seq_lengths=251):
+        batch_size = z.size(0)
+        hidden, cell = self.init_hidden(batch_size)
+
+        outputs = torch.zeros(batch_size, seq_lengths, self.dim)
+
+        inputs = z.unsqueeze(1) # Add L dim
+        for i in range(seq_lengths):
+            output, (hidden, cell) = self.lstm(inputs, (hidden, cell))
+            inputs = output
+            output = self.fc(output)
+            outputs[:, i, :] = output.squeeze(1)
+
+        return outputs
+
+class LSTMDiscriminator(_LSTM):
+    def __init__(self, h_dim=64, dim=4, num_layers=4, **kwargs):
+        super().__init__(**kwargs)
+
+        self.dim = dim
+        self.h_dim = h_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(dim, h_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(h_dim, 1)
+
+    def forward(self, inputs):
+        # hidden, cell = self.init_hidden(inputs.size(0))
+
+        output, (hidden, cell) = self.lstm(inputs)
+        output = self.fc(hidden[-1]) # Last Layers hidden states
+
+        return output
+    
---
+++ lstm_data.py
@@ -0,0 +1,89 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+MAX_LENGTH = 251
+END_TOKEN = np.array([-1, -1, 1, 1])
+PAD_TOKEN = np.array([-1, -1, -1, 1])
+
+class LSTMDataset(Dataset):
+    def __init__(self, data) -> None:
+        super().__init__()
+        self.data = self._prepare(data)
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        sequence = self.data[index]
+        return sequence
+    
+    def revert(self):
+        return self._revert(self.data)
+    
+    def _prepare(self, data):
+        size = len(data)
+        prepared = np.zeros((size, MAX_LENGTH, 4))
+        for i in range(size):
+            seq_len = data[i].shape[0]
+            prepared[i, :seq_len, :2] = (data[i][:, :2] + 500) / 1000
+            prepared[i, :seq_len, 2] = data[i][:, 2]
+            prepared[i, seq_len, :] = END_TOKEN
+            prepared[i, seq_len+1:, :] = PAD_TOKEN
+        prepared = torch.from_numpy(prepared).float()
+        return prepared
+
+    def _revert(self, data):
+        if isinstance(data, torch.Tensor):
+            data = data.detach().numpy()
+        reverted = []
+        for i in range(len(data)):
+            end_indices = (np.round(data[i, :, 3]) == 1).nonzero()[0]
+            end_index = end_indices[0] if len(end_indices) > 0 else MAX_LENGTH
+            reverted.append(data[i, :end_index, :3])
+            reverted[i][:, :2] = (reverted[i][:, :2] * 1000) - 500
+            reverted[i] = np.round(reverted[i]).astype(np.int16)
+        reverted = np.array(reverted, dtype=object)
+        return reverted
+    
+def revert(data):
+    if isinstance(data, torch.Tensor):
+        data = data.detach().numpy()
+    reverted = []
+    for i in range(len(data)):
+        end_indices = (np.round(data[i, :, 3]) == 1).nonzero()[0]
+        end_index = end_indices[0] if len(end_indices) > 0 else MAX_LENGTH
+        reverted.append(data[i, :end_index, :3])
+        reverted[i][:, :2] = (reverted[i][:, :2] * 1000) - 500
+        reverted[i] = np.round(reverted[i]).astype(np.int16)
+    reverted = np.array(reverted, dtype=object)
+    return reverted
+
+def create_mask(data):
+    end_token_index = 3
+    # Locate the end token by finding where the third component is 1
+    end_token_positions = (data[:, end_token_index] == 1).nonzero(as_tuple=True)
+    
+    if end_token_positions[0].nelement() == 0:
+        # If there is no end token, return a mask of all True
+        end_index = MAX_LENGTH
+    else:
+        # Get the index of the first end token
+        end_index = end_token_positions[0][0]
+    # Create a mask that is True up to the end token and False afterwards
+    mask = torch.arange(data.size(0)) < end_index
+    return mask.reshape(-1, 1)
+
+def create_batch_mask(data):
+    end_token_index = 3
+    
+    # Find the indices of the end token for each sequence in the batch
+    end_token_indices = (data[..., end_token_index] == 1).long().argmax(dim=1)
+
+    # Create a range tensor that broadcasts across the batch
+    sequence_range = torch.arange(data.size(1)).unsqueeze(0)
+
+    # Use broadcasting to compare the range with the indices and create the mask
+    mask = sequence_range < end_token_indices.unsqueeze(1)
+    return mask.reshape(data.size(0), -1, 1)
